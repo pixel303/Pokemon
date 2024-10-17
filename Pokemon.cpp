7f0bdd76aca814a40005b76978a8b7ddd6569547
Yash Rajput <yashrajput706@gmail.com>
2024-10-17 22:04:17 +0530

    Added Pokemon Selection
    Removed player name and greeting message and added Pokemon selection.

--- Pokemon.cpp
+++ Pokemon.cpp
@@ -1,15 +1,29 @@
 #include <iostream>
-#include <string>
 
 using namespace std;
 
 int main() {
-    string player_name;
+   
+    cout << "Welcome to the world of Pokemon! I am Professor Oak.\n";
+    cout << "You can choose one of the following Pokemon:\n";
+    cout << "1. Bulbasaur\n2. Charmander\n3. Squirtle\n";
+    cout << "Which Pokemon would you like to choose? Enter the number: ";
 
-    cout << "Welcome to the game! Please enter your name: ";
-    cin >> player_name;
+    int choice;
+    cin >> choice;
 
-    cout << "Congratulations, " << player_name << "! You're officially in the game now!" << endl;
+    if (choice == 1) {
+        cout << "You chose Bulbasaur! A wise choice.\n";
+    }
+    else if (choice == 2) {
+        cout << "You chose Charmander! A fiery choice.\n";
+    }
+    else if (choice == 3) {
+        cout << "You chose Squirtle! A cool choice.\n";
+    }
+    else {
+        cout << "Invalid choice. Please restart the game.\n";
+    }
 
     return 0;
 }
