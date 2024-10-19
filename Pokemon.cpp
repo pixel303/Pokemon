54441a95ffd891e026cffc712883f0a4854be4ce
Yash Rajput <yashrajput706@gmail.com>
2024-10-20 00:00:34 +0530

    Replaced Enums with Enum Class.
    Replaced Enums with Enum Class, assigning the player a Pokemon is now done with a different switch statement.

--- Pokemon.cpp
+++ Pokemon.cpp
@@ -3,17 +3,27 @@
 
 using namespace std;
 
-// Enum for Pokemon choices
-enum PokemonChoice {
-    Charmander = 1,  
-    Bulbasaur = 2,       
-    Squirtle = 3,       
-    InvalidChoice = 4    
+// Enum class for Pokemon choices
+enum class PokemonChoice {
+    Charmander = 1,
+    Bulbasaur = 2,
+    Squirtle = 3,
+    InvalidChoice = 4
 };
 
+// Function to convert integer input to PokemonChoice
+PokemonChoice getPokemonChoice(int choice) {
+    switch (choice) {
+    case 1: return PokemonChoice::Charmander;
+    case 2: return PokemonChoice::Bulbasaur;
+    case 3: return PokemonChoice::Squirtle;
+    default: return PokemonChoice::InvalidChoice;
+    }
+}
+
 int main() {
-    //Initialize chosen_pokemon with InvalidChoice
-    PokemonChoice chosen_pokemon = InvalidChoice;
+    // Initialize chosen_pokemon with InvalidChoice
+    PokemonChoice chosen_pokemon = PokemonChoice::InvalidChoice;
 
     cout << "Welcome to the world of Pokemon! I am Professor Oak.\n";
     cout << "I will be your guide to getting started.\n";
@@ -31,24 +41,24 @@ int main() {
     int choice;
     cin >> choice;
 
-    //Assign chosen Pokemon based on input
-    switch (choice) {
-    case Charmander:
-        chosen_pokemon = Charmander;
+    // Convert integer input to PokemonChoice
+    chosen_pokemon = getPokemonChoice(choice);
+
+    // Respond based on chosen Pokemon
+    switch (chosen_pokemon) {
+    case PokemonChoice::Charmander:
         cout << "Charmander! A fiery choice!\n";
         break;
-    case Bulbasaur:
-        chosen_pokemon = Bulbasaur;
+    case PokemonChoice::Bulbasaur:
         cout << "A fine choice! Bulbasaur goes green!\n";
         break;
-    case Squirtle:
-        chosen_pokemon = Squirtle;
+    case PokemonChoice::Squirtle:
         cout << "You chose Squirtle! Splash Splash!\n";
         break;
     default:
-        //Handle invalid choice by defaulting to Charmander
-        chosen_pokemon = Charmander;
+        // Handle invalid choice by defaulting to Charmander
         cout << "Seems like that isn't one of the choices, but don't worry! You get Charmander by default. \n";
+        chosen_pokemon = PokemonChoice::Charmander;
         break;
     }
 
