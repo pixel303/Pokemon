583ba097777c2075d0e0d98308a4125b41c3f081
Yash Rajput <yashrajput706@gmail.com>
2024-10-23 20:03:39 +0530

    Added Utility header file.
    Added Utility header files for "waitForEnter();", "clearConsole();" and "clearInputBuffer();".

--- PokemonChoice.hpp
+++ PokemonChoice.hpp
@@ -1,7 +1,12 @@
+#ifndef POKEMON_CHOICE_HPP
+#define POKEMON_CHOICE_HPP
+
 // Enum class for Pokemon choices
 enum class PokemonChoice {
     Charmander = 1,
     Bulbasaur,
     Squirtle,
-    Pikachu // Added Pikachu as a default choice
+    Pikachu 
 };
+
+#endif 
